09b4f1adb1340710f7d9f7fd1bc70b722ffcf181
Misbahul Munir <misbahul.munir@it.sharingvision.com>
2022-06-21 18:56:32 +0700

    adding session and send message when ready

--- app.js
+++ app.js
@@ -1,20 +1,37 @@
-const { Client } = require('whatsapp-web.js');
+const { Client, LocalAuth } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
 
-const client = new Client();
+const client = new Client({authStrategy: new LocalAuth()});
 
 client.on('qr', (qr) => {
     // Generate and scan this code with your phone
     console.log('QR RECEIVED', qr);
+    qrcode.generate(qr, {small: true});
 });
 
 client.on('ready', () => {
     console.log('Client is ready!');
+    client.sendMessage('6285338017331@c.us', `OK I'm ready`);
 });
 
-client.on('message', msg => {
-    if (msg.body == '!ping') {
-        msg.reply('pong');
+const greetings = ['hello', 'hi', 'ping', 'p', 'halo', 'met', 'selamat'];
+client.on('message', async msg => {
+    const contact = await msg.getContact();
+    const info = `${msg.from} - ${contact.number}: ${msg.body}`;
+    console.log(info);
+
+    const message = msg.body;
+    
+    const i = greetings.findIndex(greet => message.toLowerCase().includes(greet));
+
+    if (i >= 0) {
+        msg.reply(`${message} juga`);
     }
 });
+
+client.on('disconnected', (reason) => {
+    console.log('Client was logged out', reason);
+});
+
 
 client.initialize();
